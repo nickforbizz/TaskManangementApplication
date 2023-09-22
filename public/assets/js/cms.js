8883bbae3783689bc40568a9b398a994b5c1929d
Nicholas Waruingi <NWaruingi@kemri-wellcome.org>
2023-09-22 05:31:10 +0300

    abosolute position the loader

--- public/assets/js/cms.js
+++ public/assets/js/cms.js
@@ -1,10 +1,10 @@
 $(window).on('beforeunload', function(){
     // console.log('Are you sure you want to leave?');
     $(".navbar").fadeTo("slow",0.2);
-    $("#contentHolder").html(`<div class='text-center' style='margin-top:25vh!important'> 
-    <h1>Task Management App </h1> 
-    <h3>Loading ... </h3> 
-    </div>`).append(`
+    $("#contentHolder").html(`<div class='text-center' style='position:absolute;top:40%;left:50%;transform:translate(-50%,-50%);'> 
+        <h1>Task Management App </h1> 
+        <h3>Loading ... </h3> 
+
         <div class="text-center my_loader">
             <div class="loadingio-spinner-spinner-fpufj1c8num  ">
                 <div class="ldio-0hf3ht1mqwp">
@@ -13,5 +13,5 @@ $(window).on('beforeunload', function(){
             </div>
 
         </div>
-    `);
+    </div>`);
 });
